7baa568b843209a3aa60665af193d61f2974f10e
GaryStack <37968360+GaryStack@users.noreply.github.com>
2025-05-19 19:41:00 +0800

    Update load_MMR_V.py

--- dataset/load_MMR_V.py
+++ dataset/load_MMR_V.py
@@ -1,13 +1,12 @@
 import sys
 import os
-sys.path.append(os.path.abspath("/netdisk/zhukejian/AnyReward-master"))
 from utils import read_json
 import random
 
 def load_MMR_V_4o_error():
     
     file_paths = [
-        "/netdisk/zhukejian/implicit_video_anonotations/MMR-V - 4o - wrong.json",
+        "/netdisk/zhukejian/MMR_V/MMR-V - 4o - wrong.json",
     ]
 
     samples = None
@@ -25,24 +24,12 @@ def load_MMR_V_4o_error():
     # breakpoint()
     print(f"Load {len(samples)} samples for the text-audio-to-text preference task.")
     return samples
-    # description_file = '/netdisk/zhukejian/VCGBench-Diverse/human_annotated_video_descriptions_vcgbench_diverse_human_annotated_descriptions.json'
-    # annotation_descriptions = read_json(description_file)
-
-    # video2description = {elem["video_name"]: elem["description"] for elem in annotation_descriptions}
-    # for sample in qa_samples:
-    #     video_name = sample["video_name"]
-    #     if video_name not in video2description.keys():
-    #         # print(video_name)
-    #         continue
-    #     sample["description"] = video2description[video_name]
-    # breakpoint()
-    return IVBench_samples
 
 def load_MMR_V():
     file_paths = [
-        # "/mnt/userdata/implicit_video_anonotations/MMR-V - video -llava.json"
-        "/netdisk/zhukejian/implicit_video_anonotations/MMR-V - split.json",
-        "/mnt/userdata/implicit_video_anonotations/MMR-V - split.json"
+        # "/mnt/userdata/MMR_V/MMR-V - video -llava.json"
+        "/netdisk/zhukejian/MMR_V/MMR-V - split.json",
+        "/mnt/userdata/MMR_V/MMR-V - split.json"
     ]
 
     samples = None
@@ -60,18 +47,6 @@ def load_MMR_V():
     # breakpoint()
     print(f"Load {len(samples)} samples for MMR-V.")
     return samples
-    # description_file = '/netdisk/zhukejian/VCGBench-Diverse/human_annotated_video_descriptions_vcgbench_diverse_human_annotated_descriptions.json'
-    # annotation_descriptions = read_json(description_file)
-
-    # video2description = {elem["video_name"]: elem["description"] for elem in annotation_descriptions}
-    # for sample in qa_samples:
-    #     video_name = sample["video_name"]
-    #     if video_name not in video2description.keys():
-    #         # print(video_name)
-    #         continue
-    #     sample["description"] = video2description[video_name]
-    # breakpoint()
-    return IVBench_samples
 
 
 
